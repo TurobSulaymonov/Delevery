334a263d88b3b510b3ecc314f896f5283df64b04
TurobSulaymonov <turobsulaymonov117@gmail.com>
2025-11-16 18:40:48 +0900

    feat: develop register && login rest api business logic

---
+++ apps/user/src/user/user.service.ts
@@ -0,0 +1,44 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      throw new BadRequestException('이미 가입된 이매일 입니다!');
+    }
+
+    console.log('user1:', user);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await this.userRepository.save({
+      ...createUserDto,
+      email,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      password: hashedPassword,
+    });
+
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}

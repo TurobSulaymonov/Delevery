0c0ec27c25971ed003faa8f816ee3da4544e7825
TurobSulaymonov <turobsulaymonov117@gmail.com>
2025-11-18 19:31:35 +0900

    feat: develop notification microservice busines logic

--- apps/notification/src/app.module.ts
+++ apps/notification/src/app.module.ts
@@ -1,15 +1,13 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { OrderModule } from './order/order.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        HTTP_PORT: Joi.string().required(),
         DB_URL: Joi.string().required(),
       }),
     }),
@@ -20,8 +18,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService],
     }),
-
-    OrderModule,
   ],
 })
 export class AppModule {}
---
+++ apps/notification/src/notification/entity/notification.entity.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+
+export enum NotificationStatus {
+  pending = 'Pending',
+  sent = 'Sent',
+}
+@Schema()
+export class Notification extends Document {
+  @Prop({
+    required: true,
+  })
+  from: string;
+
+  @Prop({
+    required: true,
+  })
+  to: string;
+
+  @Prop({
+    required: true,
+  })
+  subject: string;
+
+  @Prop({
+    required: true,
+  })
+  content: string;
+
+  @Prop({
+    enum: NotificationStatus,
+    default: NotificationStatus.pending,
+  })
+  notificationStatus: NotificationStatus;
+}
